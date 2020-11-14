5624b76fa613bda04deb453c8771433671067391
willh33 <wharrison33@hotmail.com>
2020-11-14 15:37:49 -0700

    fixed

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ const questions = [
     },
     {
       type: 'input',
-      name: 'contibute',
+      name: 'contribute',
       message: 'What does the user need to to know about contributing to the repo?',
     },
 ];
@@ -79,42 +79,45 @@ init();
 
 const generateText = ( {username, email, project, description, license, dependencies, tests, usage, contribute} ) => {
   return `
-      # Project Title
-      ${project}
-      
-      ${licenses[license]}
-      
-      ## Description
-      ${description}
-      
-      ## Table of Contents
-      * [Installation](#installation)
-      * [Usage](#usage)
-      * [License](#license)
-      * [Contributing](#contributing)
-      * [Tests](#tests)
-      * [Questions](#questions)
-      
-      ##Installation
-      To install the necessary dependencies, run the following command
-      \`${dependencies}\`
-      
-      ##Usage
-      ${usage}
-      
-      
-      ##License
-      This project is licensed under ${license}
-      
-      ##Contributing
-      ${contribute}
-      
-      ##Tests
-      To run tests, run the following command
-      \`${tests}\`
-      
-      ##Questions
-      If you have questions about the repo, open an issue or contact me directly at ${email}. You can find more of my work at ${username}
+# Project Title
+${project}
+
+${licenses[license]}
+
+## Description
+${description}
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+##Installation
+To install the necessary dependencies, run the following command
+
+\`${dependencies}\`
+
+##Usage
+
+${usage}
+
+
+##License
+This project is licensed under ${license}
+
+##Contributing
+${contribute}
+
+##Tests
+To run tests, run the following command
+
+\`${tests}\`
+
+##Questions
+If you have questions about the repo, open an issue or contact me directly at ${email}. You can find more of my work at ${username}
   `
   
 }
